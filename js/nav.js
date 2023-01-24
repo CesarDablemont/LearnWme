406ef7be4da65e309801051ec0f9c48432ef38b1
Cesar <cesar.dablemont@gmail.com>
2023-01-24 17:51:59 +0100

    update

---
+++ js/nav.js
@@ -0,0 +1,27 @@
+var dataDropdown = "";
+
+function toggle(id)
+{
+  document.getElementById(id).classList.toggle("show");
+  if(dataDropdown != "" && id != dataDropdown)
+  {
+    document.getElementById(dataDropdown).classList.toggle("show");
+  }
+  dataDropdown = id;
+}
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!(event.target.matches('.dropbtn') || event.target.matches('.dropdown-content'))) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
